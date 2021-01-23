3823e8644edc07e2d9e189824b2be9e0d83e571f
kravchenkoevgen <kravchenkoevgen@nvk6.ukr.education>
2021-01-23 18:02:55 +0200

    tasks 3 and 7

--- acmp/0007.py
+++ acmp/0007.py
@@ -1,8 +1,8 @@
 fin  = open("input.txt")
 fout = open("output.txt","w")
 
-a = int(fin.readline())
-fout.write(str(a * a))
+a, b, c = map(int, fin.readline().split())
+fout.write(str(max(a, b, c)))
 
 fin.close()
 fout.close()
