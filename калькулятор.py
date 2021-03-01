9a544202e9308549206db5679c6be56272e2b348
kravchenkoevgen <kravchenkoevgen@nvk6.ukr.education>
2021-03-01 18:01:19 +0200

    Зробив калькулятор.

---
+++ калькулятор.py
@@ -0,0 +1,23 @@
+what = input('Що робим? (-, +, *, /, %, **):')
+a = float(input('Яке перше число?:'))
+b = float(input('Яке друге число?:'))
+if what == '-':
+    c = a - b
+    print(c)
+elif what == '+':
+    c = a + b
+    print(c)
+elif what == '*':
+    c = a * b
+    print(c)
+elif what == '/':
+    c = a / b
+    print(c)
+elif what == '%':
+    c = a % b
+    print(c)
+elif what == '**':
+    c = a ** b
+    print(c)
+else:
+    print('Введина не правильна операція')
